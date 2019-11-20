b4c437e8bf4832d8014b14cfabe3227a69ff169b
carloscalderon <carloscalderon@hotmail.com>
2019-11-20 00:11:23 -0500

    EJERCICIO#1 ECUACION DE 2 GRADO

--- AppEcuacionesX/Form1.Designer.cs
+++ AppEcuacionesX/Form1.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.btnCALCULAR = new System.Windows.Forms.Button();
+            this.btnNUEVO = new System.Windows.Forms.Button();
+            this.btnSALIR = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ECUACIONE DE 2 GRADO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "INGRESAR VALOR A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "INGRESAR VALOR B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "INGRESAR VALOR C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "X1";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(166, 73);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 5;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(166, 122);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 6;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(166, 168);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 7;
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(166, 209);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.ReadOnly = true;
+            this.txtX1.Size = new System.Drawing.Size(100, 20);
+            this.txtX1.TabIndex = 8;
+            // 
+            // btnCALCULAR
+            // 
+            this.btnCALCULAR.Location = new System.Drawing.Point(329, 93);
+            this.btnCALCULAR.Name = "btnCALCULAR";
+            this.btnCALCULAR.Size = new System.Drawing.Size(75, 23);
+            this.btnCALCULAR.TabIndex = 9;
+            this.btnCALCULAR.Text = "CALCULAR";
+            this.btnCALCULAR.UseVisualStyleBackColor = true;
+            this.btnCALCULAR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNUEVO
+            // 
+            this.btnNUEVO.Location = new System.Drawing.Point(329, 139);
+            this.btnNUEVO.Name = "btnNUEVO";
+            this.btnNUEVO.Size = new System.Drawing.Size(75, 23);
+            this.btnNUEVO.TabIndex = 10;
+            this.btnNUEVO.Text = "NUEVO";
+            this.btnNUEVO.UseVisualStyleBackColor = true;
+            // 
+            // btnSALIR
+            // 
+            this.btnSALIR.Location = new System.Drawing.Point(329, 185);
+            this.btnSALIR.Name = "btnSALIR";
+            this.btnSALIR.Size = new System.Drawing.Size(75, 23);
+            this.btnSALIR.TabIndex = 11;
+            this.btnSALIR.Text = "SALIR";
+            this.btnSALIR.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "X2";
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(166, 257);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.ReadOnly = true;
+            this.txtX2.Size = new System.Drawing.Size(100, 20);
+            this.txtX2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(422, 307);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSALIR);
+            this.Controls.Add(this.btnNUEVO);
+            this.Controls.Add(this.btnCALCULAR);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculo ecuacion de 2 grado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.Button btnCALCULAR;
+        private System.Windows.Forms.Button btnNUEVO;
+        private System.Windows.Forms.Button btnSALIR;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtX2;
     }
 }
 
--- AppEcuacionesX/Form1.cs
+++ AppEcuacionesX/Form1.cs
@@ -16,5 +16,22 @@ namespace AppEcuacionesX
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, r1,r2 ;
+            a = Double.Parse(this.txtA.Text);
+            b = Double.Parse(this.txtB.Text);
+            c = Double.Parse(this.txtC.Text);
+            //LLAMA LA FUNCION QUE CALCULA X1
+            r1 = AppEcuaciones.Ecuaciones.Ecupositiva(a, b, c);
+            //LLAMA LA FUNCION QUE CALCULA X2
+            r2 = AppEcuaciones.Ecuaciones.Ecunegativa(a, b, c);
+
+            this.txtX1.Text= r1.ToString();
+            this.txtX2.Text = r2.ToString();
+
+
+        }
     }
 }
