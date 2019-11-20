7a76502950f1f6afffee48c5a8dc78d1d70185c6
carloscalderon <carloscalderon@hotmail.com>
2019-11-19 23:25:09 -0500

    Agregar archivos de proyecto.

---
+++ AppEcuaciones/Ecuaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEcuaciones
+{
+    public static class Ecuaciones
+    {
+        public static double Ecupositiva(double a, double b, double c)
+        {
+            double resul = ((-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a));
+            return resul;
+        }
+
+
+    }
+}
